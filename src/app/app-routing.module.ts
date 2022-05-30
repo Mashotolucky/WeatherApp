b40ba817829e45b57df0da61997adb04a3a8d493
Mashotolucky <mashotolucky99@gmail.com>
2022-05-30 20:42:50 +0200

    wireframes done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForecastComponent } from './components/forecast/forecast.component';
 import { LocationComponent } from './components/location/location.component';
 
-const routes: Routes = [{path: '', component: LocationComponent}];
+const routes: Routes = [{path: 'home', component: LocationComponent},
+                            {path: '', component: ForecastComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
