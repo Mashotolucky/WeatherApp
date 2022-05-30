476ae77fdff0e6008cca8828f3d4d674179139f6
Mashoto Molefe <mashoto.molefe@dadevs.co.za>
2022-05-30 16:16:51 +0200

    Weather app first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LocationComponent } from './components/location/location.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', component: LocationComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LocationComponent } from './components/location/location.component';
+import { ForecastComponent } from './components/forecast/forecast.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocationComponent,
+    ForecastComponent
   ],
   imports: [
     BrowserModule,
